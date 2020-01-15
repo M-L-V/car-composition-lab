02b2eb667c7b77cc42a36326470df8405cabf616
Morgaine <morgainedv@gmail.com>
2020-01-15 13:45:26 +0000

    adds electric car getter test

--- src/test/java/ElectricCarTest.java
+++ src/test/java/ElectricCarTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+import vehicle.vehicleTypes.ElectricCar;
+
+import static org.junit.Assert.assertEquals;
+
 public class ElectricCarTest {
+
+    private ElectricCar electricCar;
+
+    @Test
+    public void testElectricCar(){
+        electricCar = new ElectricCar(300.50, "green");
+        assertEquals(300.5, electricCar.getPrice(), 0.01);
+        assertEquals("green", electricCar.getColour());
+
+    }
+
 }
