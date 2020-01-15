8a621377a02b01749c01eed93ae79838c27fe1d4
Morgaine <morgainedv@gmail.com>
2020-01-15 13:47:42 +0000

    adds hybrid car getter tests

--- src/test/java/HybridCarTest.java
+++ src/test/java/HybridCarTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+import vehicle.vehicleTypes.HybridCar;
+
+import static org.junit.Assert.assertEquals;
+
 public class HybridCarTest {
+
+    private HybridCar hybridCar;
+
+    @Test
+    public void testHybridCar(){
+        hybridCar = new HybridCar(350.20, "blue");
+        assertEquals(350.2, hybridCar.getPrice(), 0.01);
+        assertEquals("blue", hybridCar.getColour());
+
+    }
+
 }
